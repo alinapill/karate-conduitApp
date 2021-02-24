148d0d30d118ac2c48a70a3f0adc5afabd40ed5b
Alina Pila <alina.pila@gmail.com>
2021-02-24 16:55:33 +0200

    add CreateToken.feature file. Add Environment variables.

---
+++ README.md
@@ -0,0 +1,4 @@
+# Test suite for Conduit app
+
+### Run tests that contains specific tag
+mvn test -Dtest=ConduitTest#testTags
--- src/test/java/conduitApp/ConduitTest.java
+++ src/test/java/conduitApp/ConduitTest.java
@@ -10,5 +10,5 @@ class ConduitTest {
     Karate testAll() {
         return Karate.run().relativeTo(getClass());
     }
-    
 }
+
